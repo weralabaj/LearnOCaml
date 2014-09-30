8c879627df4844ab35239f1c3ee2a80783e9974b
weronika.labaj <weronika.labaj@gmail.com>
2014-09-30 19:27:01 +1300

    Solutions for few more problems

--- 99problems/99problems.ml
+++ 99problems/99problems.ml
@@ -21,3 +21,33 @@ let length l =
 
 let length_fold l =
 	List.fold_left (+) 0 l;;
+
+let rev l =
+	let rec internal acc = function
+		| [] -> acc
+		| h::tail -> internal (h::acc) tail
+	in internal [] l
+
+let isPalindrome l =
+	l = rev l
+
+type 'a node =
+    | One of 'a 
+    | Many of 'a node list
+
+let flatten l =
+	let rec aux acc = function
+		| [] -> acc
+		| One a::t -> aux (a::acc) t
+		| Many a::t -> aux (aux acc a) t
+	in List.rev (aux [] l)
+
+let rec compress = function
+		| a::(b::t) -> 
+			if a=b then 
+				compress (b::t) 
+			else a :: compress (b::t)
+		| x -> x
+		
+
+
